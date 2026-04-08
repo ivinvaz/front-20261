6426e5c62d0641ecf79f6a56f7ad1d6e2564c6ea
Ivin <vaz.ivin@gmail.com>
2026-04-07 21:59:46 -0300

    Adicionando páginas iniciais do exercício 03

--- praticas/aluno-online-react/pages/dashboard.jsx
+++ praticas/aluno-online-react/pages/dashboard.jsx
@@ -6,6 +6,11 @@ import Aside from "../components/Aside";
 function App() {
   return <div className="flex flex-col md:flex-row w-full min-h-screen bg-gray-50">
     <Aside className="flex-none md:flex-1"/>
+    <main className="flex-1 flex-col">
+      <Header/>
+      <Card/>
+      <Card/>
+    </main>
   </div>;
 }
 
