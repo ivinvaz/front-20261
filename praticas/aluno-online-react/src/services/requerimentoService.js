0de34097fef04026d59d8d61b12bba3d599ff02f
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2026-06-11 13:22:13 -0300

    Adicionando services

--- praticas/aluno-online-react/src/services/requerimentoService.js
+++ praticas/aluno-online-react/src/services/requerimentoService.js
@@ -1,5 +1,29 @@
-async function listar(){}
+const url = "http://localhost:3000/produtos";
 
-async function cadastrar(){}
+async function listar(){
+    try{
+        const resposta = await fetch(url, {
+            method:"GET",
+        });
+        const dados = await resposta.json();
+        return dados;
+    }catch(error){ 
+        return console.log("Deu ruim!",error.message)
+    }
+}
+
+async function cadastrar(produto){
+    try{
+        const resposta = await fetch(url, {
+            method:"POST",
+            body:JSON.stringify(produto),
+            headers: {"content-type":"application/json"}
+        });
+        const dados = await resposta.json();
+        return dados;
+    }catch(error){ 
+        return console.log("Deu ruim!",error.message)
+    }
+}
 
 export default { listar, cadastrar }
